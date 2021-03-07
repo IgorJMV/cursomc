0126faf4da0e41f604a1e04abc387ff24e235b3d
Igor Marinho <igorjmv2000@gmail.com>
2021-03-06 21:25:48 -0300

    Criando repository e service para Categoria

--- src/main/java/com/igorjmv2000/gmail/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/igorjmv2000/gmail/cursomc/resources/CategoriaResource.java
@@ -1,27 +1,34 @@
 package com.igorjmv2000.gmail.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.igorjmv2000.gmail.cursomc.domain.Categoria;
+import com.igorjmv2000.gmail.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
+	@Autowired
+	private CategoriaService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Categoria>> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> list = new ArrayList<>(Arrays.asList(cat1, cat2));
+	public ResponseEntity<List<Categoria>> findAll() {
+		List<Categoria> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+		Categoria obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
