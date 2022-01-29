3693e1a329974dd11611b65ad5f4be243b233c6d
IgorJMV <igorjmv2000@gmail.com>
2022-01-29 20:48:34 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/igormarinho/cursomc/domain/Categoria.java
+++ src/main/java/com/igormarinho/cursomc/domain/Categoria.java
@@ -2,11 +2,20 @@ package com.igormarinho.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+@Entity
+@Table(name = "tb_categoria")
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +25,8 @@ import lombok.Setter;
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Integer id;
 	private String nome;
