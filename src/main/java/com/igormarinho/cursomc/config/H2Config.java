2cad3d50a9527e60d517e22fd32ee0cccd96d64d
IgorJMV <igorjmv2000@gmail.com>
2022-02-01 17:47:58 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/igormarinho/cursomc/config/H2Config.java
+++ src/main/java/com/igormarinho/cursomc/config/H2Config.java
@@ -65,8 +65,14 @@ public class H2Config implements CommandLineRunner{
 		//Categoria
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		Categoria cat8 = new Categoria(null, "Aviação");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		
 		//Produtos
 		Produto p1 = new Produto(null, "Computador", 2000d);
--- src/main/java/com/igormarinho/cursomc/services/CategoriaService.java
+++ src/main/java/com/igormarinho/cursomc/services/CategoriaService.java
@@ -2,14 +2,15 @@ package com.igormarinho.cursomc.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.igormarinho.cursomc.domain.Categoria;
-import com.igormarinho.cursomc.dto.CategoriaDTO;
 import com.igormarinho.cursomc.repositories.CategoriaRepository;
 import com.igormarinho.cursomc.services.exceptions.DataIntegrityException;
 import com.igormarinho.cursomc.services.exceptions.ObjectNotFoundException;
@@ -50,6 +51,11 @@ public class CategoriaService {
 		}catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que tenha produtos.");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 	
 }
