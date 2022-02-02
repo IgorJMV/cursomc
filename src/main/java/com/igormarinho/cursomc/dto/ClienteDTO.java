064af1f492cd02987e5794145a1ac9596eacc865
IgorJMV <igorjmv2000@gmail.com>
2022-02-02 17:52:10 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/igormarinho/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/igormarinho/cursomc/dto/ClienteDTO.java
@@ -7,10 +7,13 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.igormarinho.cursomc.domain.Cliente;
+import com.igormarinho.cursomc.services.validation.ClienteUpdate;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+@ClienteUpdate
 
 @NoArgsConstructor
 @Getter
