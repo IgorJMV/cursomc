9cce6a9cb4e6fea331b529744a1d9fa4f74abd0e
IgorJMV <igorjmv2000@gmail.com>
2022-02-02 01:54:01 -0300

    PUT, DELETE, GET para Cliente

--- src/main/java/com/igormarinho/cursomc/services/ClienteService.java
+++ src/main/java/com/igormarinho/cursomc/services/ClienteService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.igormarinho.cursomc.domain.Cliente;
+import com.igormarinho.cursomc.dto.ClienteDTO;
 import com.igormarinho.cursomc.repositories.ClienteRepository;
+import com.igormarinho.cursomc.services.exceptions.DataIntegrityException;
 import com.igormarinho.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,4 +35,32 @@ public class ClienteService {
 		);
 	}
 	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
