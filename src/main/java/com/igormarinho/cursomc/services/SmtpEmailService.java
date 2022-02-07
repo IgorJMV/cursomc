f95c0161be4dd3a3c8cc45cf382d33be36f29111
IgorJMV <igorjmv2000@gmail.com>
2022-02-07 18:15:26 -0300

    Email HTML

--- src/main/java/com/igormarinho/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/igormarinho/cursomc/services/SmtpEmailService.java
@@ -1,8 +1,11 @@
 package com.igormarinho.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import lombok.extern.java.Log;
 
@@ -12,10 +15,20 @@ public class SmtpEmailService extends AbstractEmailService {
 	@Autowired
 	private MailSender mailSender;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		log.info("Enviando de email...");
 		mailSender.send(msg);
+		log.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		log.info("Enviando de email...");
+		javaMailSender.send(msg);
 		log.info("Email enviado");
 	}
 
