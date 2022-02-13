dbfa187d2349e7fba29a550579c4b26dd8aaf492
IgorJMV <igorjmv2000@gmail.com>
2022-02-13 20:25:52 -0300

    Enviando imagem via endpoint

--- src/main/java/com/igormarinho/cursomc/CursomcApplication.java
+++ src/main/java/com/igormarinho/cursomc/CursomcApplication.java
@@ -1,25 +1,13 @@
 package com.igormarinho.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.igormarinho.cursomc.services.S3Service;
-
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
+public class CursomcApplication{
 	
-	@Autowired
-	private S3Service s3Service;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\fotos\\spring.png");
 	}
 
 }
