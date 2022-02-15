70860090e60a433d7669ae185ad67de13c67de16
IgorJMV <igorjmv2000@gmail.com>
2022-02-15 00:30:28 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/igormarinho/cursomc/domain/Cliente.java
+++ src/main/java/com/igormarinho/cursomc/domain/Cliente.java
@@ -70,6 +70,9 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	@Getter
 	private List<Pedido> pedidos = new ArrayList<>();
+	
+	@Getter @Setter
+	private String imageUrl;
 
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
